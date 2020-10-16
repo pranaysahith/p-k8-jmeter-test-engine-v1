45971e212c22850d315a51bd5c95e5278c79bfa5
Harut Grigoryan <harut.grigoryan@4r-soft.com>
2020-10-16 17:24:05 +0400

    Pass JVM_ARGS with manifest definitions

--- src/controller/docker-jmeter-c-icap/Test/launch.sh
+++ src/controller/docker-jmeter-c-icap/Test/launch.sh
@@ -7,17 +7,17 @@
 
 set -e
 freeMem=`awk '/MemFree/ { print int($2/1024) }' /proc/meminfo`
-s=$(($freeMem/10*8))
-x=$(($freeMem/10*8))
-n=$(($freeMem/10*2))
-export JVM_ARGS="-Xmn128m -Xms512m -Xmx512m"
+#s=$(($freeMem/10*8))
+#x=$(($freeMem/10*8))
+#n=$(($freeMem/10*2))
+#export JVM_ARGS="-Xmn${n}m -Xms${s}m -Xmx${x}m"
 
 T_DIR=/usr/share/Test
 R_DIR=${T_DIR}/report
 rm -rf ${R_DIR} > /dev/null 2>&1
 mkdir -p ${R_DIR}
 
-#echo "START Running Jmeter on `date`"
+echo "START Running Jmeter on `date`"
 echo "JVM_ARGS=${JVM_ARGS}"
 
 /usr/local/apache-jmeter-5.3/bin/jmeter -n \
